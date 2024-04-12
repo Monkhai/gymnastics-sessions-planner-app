5d19e2a9d545851c407c730639e3cc2d78bbdc0c
Yohai Wiener <yohaiwiener@gmail.com>
2024-04-12 18:35:11 +0300

    log in implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { RectButton } from '@/Components/GeneralComponents/Buttons';
+import { jwtDecode } from 'jwt-decode';
+import { AnimatedPressable, RectButton } from '@/Components/GeneralComponents/Buttons';
 import { EmphasizedTitleText } from '@/Components/GeneralComponents/Texts';
 import { LabeledTextInput } from '@/Components/Lists/LabeledTextInput';
 import Colors from '@/Constants/Colors';
@@ -7,16 +8,18 @@ import logoDark from '@/assets/logo/logo_dark.png';
 import { supabase } from '@/config/initSupabase';
 import { FasterImageView } from '@candlefinance/faster-image';
 import React from 'react';
-import { Image, StyleSheet, View, useColorScheme } from 'react-native';
+import { Image, Keyboard, StyleSheet, View, useColorScheme } from 'react-native';
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import Animated, { FadeIn } from 'react-native-reanimated';
 
 const LOGO = Image.resolveAssetSource(logo).uri;
 const LOGO_DARK = Image.resolveAssetSource(logoDark).uri;
 
 const Login = () => {
   GoogleSignin.configure({
-    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-    //  webClientId: '<FROM DEVELOPER CONSOLE>',
+    // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+    webClientId: '903093166492-gc7ql9a3j34n5mi8sfv436oqjf9jvdi1.apps.googleusercontent.com',
+    iosClientId: '903093166492-vvrddauiobk4ovsp7lk3irl8vs56er63.apps.googleusercontent.com',
   });
   const colorScheme = useColorScheme();
 
@@ -50,7 +53,9 @@ const Login = () => {
   };
 
   return (
-    <View
+    <AnimatedPressable
+      onPress={() => Keyboard.dismiss()}
+      entering={FadeIn.duration(500)}
       style={{
         flex: 1,
         justifyContent: 'center',
@@ -68,6 +73,41 @@ const Login = () => {
       </View>
 
       <View style={{ gap: 16, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Wide}
+          color={colorScheme === 'dark' ? GoogleSigninButton.Color.Dark : GoogleSigninButton.Color.Light}
+          onPress={async () => {
+            try {
+              await GoogleSignin.hasPlayServices();
+              const userInfo = await GoogleSignin.signIn();
+
+              if (userInfo.idToken) {
+                const { data, error } = await supabase.auth.signInWithIdToken({
+                  provider: 'google',
+                  token: userInfo.idToken,
+                });
+                console.log('data: ', data);
+                console.log('error: ', error);
+              } else {
+                console.log('no id token');
+              }
+            } catch (error: any) {
+              if (error.cod === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+                console.log('cancelled');
+              } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('in progress');
+              } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log('play services not available or outdated');
+              } else {
+                console.log('error', error);
+              }
+            }
+          }}
+        />
+      </View>
+
+      <View style={{ gap: 16, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
         <LabeledTextInput label="Email" value={email} onChangeText={setEmail} placeholder="Email" keyboardType="default" />
         <LabeledTextInput
           label="Password"
@@ -81,10 +121,9 @@ const Login = () => {
 
       <View style={{ gap: 16, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
         <RectButton label="Login" wide onPress={handleSignIn} />
-
         <RectButton secondary label="Create Acount" onPress={handleSingUp} wide />
       </View>
-    </View>
+    </AnimatedPressable>
   );
 };
 
